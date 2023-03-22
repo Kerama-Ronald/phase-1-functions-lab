39666fb7796e85c23db7ef6471ab5780fe50b281
Ronald Kerama <keramaronald11@gmail.com>
2023-03-22 08:02:59 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(value) {
+    return Math.abs(value - 42);
+  }
+  
+  function distanceFromHqInFeet(value) {
+    return distanceFromHqInBlocks(value) * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(destination - start);
+    return blocks * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const feet = distanceTravelledInFeet(start, destination);
+    let price = 0;
+    if (feet <= 400) {
+      price = 0;
+    } else if (feet <= 2000) {
+      price = (feet - 400) * 0.02;
+    } else if (feet <= 2500) {
+      price = 25;
+    } else {
+      return 'cannot travel that far';
+    }
+    return price;
+  }
+  
